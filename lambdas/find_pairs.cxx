38bee7962154d04f97123969eccd1dc7100238ec
Bryan <kanubu@mail.uc.edu>
2015-07-16 16:06:43 -0400

    Added Author

--- lambdas/find_pairs.cxx
+++ lambdas/find_pairs.cxx
@@ -1,30 +1,31 @@
-    #include <vector>
-    #include <iostream>
-    #include <algorithm>
-    #include <functional>
-     
-    int main()
-    {
-        std::vector<int> c { 1,2,3,4,5,6,7 };
-        std::vector<int> d { 10, 20, 30, 40, 50 };
-        std::vector<int> e;
-        int x = 5;
-     
-        // auto check = [](int n, int x) { return n > x; };
-        auto check2 = [](int n, std::vector<int> x,  std::vector<int> y ) 
+//Author: Bryan Kanu
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <functional>
+ 
+int main()
+{
+    std::vector<int> c { 1,2,3,4,5,6,7 };
+    std::vector<int> d { 10, 20, 30, 40, 50 };
+    std::vector<int> e;
+    int x = 5;
+ 
+    // auto check = [](int n, int x) { return n > x; };
+    auto check2 = [](int n, std::vector<int> x,  std::vector<int> y ) 
+    { 
+        for(auto i: x)
         { 
-            for(auto i: x)
-            { 
-                for(auto l: y)
-                    if((i*i) +  n > l) std::cout << i * i + n << " > " << l << "\n";
-            } 
-     
-        };
-     
-        std::cout << "c: ";
-        for (auto i: c) {
-            check2(i, c, d) ;
-        }
-        std::cout << '\n';
-     
+            for(auto l: y)
+                if((i*i) +  n > l) std::cout << i * i + n << " > " << l << "\n";
+        } 
+ 
+    };
+ 
+    std::cout << "c: ";
+    for (auto i: c) {
+        check2(i, c, d) ;
     }
+    std::cout << '\n';
+ 
+}
