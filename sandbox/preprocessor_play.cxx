e91dfcf129ee40642c84d4fb3d7ace3e1bd0ae9f
Bryan <kanubu@mail.uc.edu>
2015-10-15 20:02:40 -0400

    Updated directives...incomplete

--- sandbox/preprocessor_play.cxx
+++ sandbox/preprocessor_play.cxx
@@ -3,22 +3,42 @@
 
 #define multiply( f1, f2 ) ( f1 * f2 )
 #define store(a1, a2) (a1 = a2)
-#define a(name) std::string (a = name)
+#define ask(typ, a) void add##typ() { std::cout<< a;}
+ask(Num, "Enter a number: ")
+ask(Word, "Enter a string: ")
+ask(More, "More to add? 1 or 0: ")
+#undef ask
 using namespace std;
 
 //this program was created using ideone, so the input values are usually given as input on the webpage
 
 int main() 
 {
-    int z, y;
+    int z, y, q;
     std::string p, v;
-    std::cin >> z >> y >> p;
-    auto x = multiply(z, y);
-    store(v, p);
-    std::cout << z << " * " << y << ": " << x << "\n";
-    std::cout << v << "\n";
     std::string a;
-    a("apple");
-    std::cout << a << "\n";
+    bool more = true;
+    do{
+        addNum();
+        std::cin >> z;
+        addNum();
+        std::cin >> y;
+
+        addWord();
+        std::cin >> p;
+        auto x = multiply(z, y);
+        store(v, p);
+        std::cout << z << " * " << y << ": " << x << "\n";
+        std::cout << v << "\n";
+
+        //addWord();
+        /*give("apple");
+        std::cout << give << "\n";
+        //give(p);*/
+        addMore();
+        std::cin >> q;
+        more = q;
+
+    }while(more);
     return 0;
 }
