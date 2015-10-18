2c1d8c5cc8b98222d81c83c3c303d23c41efbfc2
Bryan <kanubu@mail.uc.edu>
2015-10-18 14:06:05 -0400

    added do-while

--- sandbox/rock_paper_scissors.cxx
+++ sandbox/rock_paper_scissors.cxx
@@ -7,8 +7,11 @@
 #define func(n,a) void me##n() { std::cout << a;} 
 func(nu, "Enter rock, paper or scissors: ")
 #undef func
+#define func(n, a) void ask##n() { std::cout << a;}
+func(PlayAgain, "Would you like to play again? [y or n]: ")
 using namespace std;
 
+char opts[] = {'n', 'y'};
 string choice[] = {"rock", "paper", "scissors"
 , "paper","rock", "scissors"
 , "scissors", "rock", "paper"
@@ -16,15 +19,25 @@ string choice[] = {"rock", "paper", "scissors"
 , "rock", "scissors", "paper"
 , "paper", "scissors", "rock"};
 enum {rock, paper, scissors};
+enum {n, y};
+
 
 int main()
 {
-    
-    menu();
-    string userChoice;
-    cin >> userChoice;
-    srand(time(NULL));
-    cout << "User: "<< userChoice <<"\nComputer: " << choice[rand() % 18] << "\n";
+    bool playAgain = true;
+    char userWantsToPlayAgain;
+    do
+    {
+        menu();
+        string userChoice;
+        cin >> userChoice;
+        srand(time(NULL));
+        cout << "User: "<< userChoice <<"\nComputer: " << choice[rand() % 18] << "\n";
+        askPlayAgain();
+        std::cin >> userWantsToPlayAgain;
+        playAgain = (userWantsToPlayAgain == opts[n]) ? false : true;
+    }while(playAgain);
+
 
 
     return 0;
