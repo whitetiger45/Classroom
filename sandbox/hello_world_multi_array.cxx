12801561934ca93f0a507ee49a105679d3ef036a
Bryan <kanubu@mail.uc.edu>
2015-10-20 20:25:54 -0400

    unfinished...extra letter added

--- sandbox/hello_world_multi_array.cxx
+++ sandbox/hello_world_multi_array.cxx
@@ -5,10 +5,11 @@ using namespace std;
 
 char hello [HEIGHT][WIDTH];
 char world [HEIGHT][WIDTH];
-char word1 [] = { 'h', 'e', 'l', 'l', 'o'};
+char word1 [] = {'h', 'e', 'l', 'l', 'o'};
 char word2 [] = {'w', 'o', 'r', 'l', 'd'};
 int n,m;
-
+int x = 0;
+int y = HEIGHT-1;
 int main ()
 {
   for (n=0; n<HEIGHT; n++)
@@ -16,14 +17,28 @@ int main ()
     {
       if(n == m)
         hello[n][m]= word1[n];
+      else if(m == y)
+      hello[n][y--]=word2[n];
     }
   
+
     for (n=HEIGHT; n >=0; n--)
-        for (m=WIDTH; m >=0; m--)
+      for (m=WIDTH; m >=0; m--)
+      {
+        if(n == m)
         {
-          if(n == m)
-            world[n][m]= word2[m];
+          world[n][m]= word2[m];
         }
+        else if(m == x)
+        {
+          if(x == 0)
+          {
+            x++;
+            continue;
+          }
+        world[n][x++]=word1[n];
+        }
+      }
     
     
   for (n=0; n<HEIGHT; n++)
