905b485f5b39c42a2076a2b7c76c7eb9b1fc2961
Bryan <kanubu@mail.uc.edu>
2015-10-25 16:07:51 -0400

    fixed hangman output

--- sandbox/hangman.cxx
+++ sandbox/hangman.cxx
@@ -3,7 +3,9 @@ using namespace std;
 
 int main() 
 {
-    cout<< "______\n";
-    cout<< "|    |\n|    o\n|   /|\\ \n|   / \\  \n|______\n";
+    cout<< " ______\n";
+    cout<< "|    |\n|    o\n|   /|\\\n|   / \\  \n|______\n";
+
+    //for(int i = 0)
     return 0;
 }
