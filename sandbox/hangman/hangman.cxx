293d6df2af86986d58bd2eaf053b24519d024b84
Bryan <kanubu@mail.uc.edu>
2015-11-09 19:54:34 -0500

    fixed scoreboard display

--- sandbox/hangman/hangman.cxx
+++ sandbox/hangman/hangman.cxx
@@ -26,8 +26,19 @@ func(UserWinsRound, "You win!\nThe correct word was: ", userScore, totalGames)
 func(OutOfGuesses, "Sorry, you are out of guesses...\nThe correct word was: ", totalGames)
 #undef func
 
-#define func(n, a, b) void score##n(){ std::cout << "\nWins | Total Games Played This Round|\n  " << a << "      |            " << b << "             |   \n";}
+#define func(n, a, b) void score##n(){ std::cout << "\nWins | Total Games Played This Round|\n  " << a << "  |                " << b << "             |   \n";}
 func(Board, userScore, totalGames)
+
+//todo: 
+//look into passing a string with the word to the function so it outputs the word in the same method that updates the scoreboard
+// int call_a_func(MathFunc call_this, int x, int y) 
+// {
+//     float output = call_this(x, y);
+//     return output;
+// }
+// int final_result = call_a_func(&mul, a, b);
+// cout<< a << " * " << b << " = " << final_result << "\n";
+
 
 char response[] = {'n', 'y'};
 enum {n, y};
