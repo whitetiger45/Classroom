d4c75b4b0dce13f11ee6be8423ceec770f8037c3
Bryan <kanubu@mail.uc.edu>
2015-11-09 19:50:50 -0500

    removed comments

--- sandbox/hangman/hangman.cxx
+++ sandbox/hangman/hangman.cxx
@@ -29,17 +29,6 @@ func(OutOfGuesses, "Sorry, you are out of guesses...\nThe correct word was: ", t
 #define func(n, a, b) void score##n(){ std::cout << "\nWins | Total Games Played This Round|\n  " << a << "      |            " << b << "             |   \n";}
 func(Board, userScore, totalGames)
 
-//todo: 
-//look into passing a string with the word to the function so it outputs the word in the same method that updates the scoreboard
-// int call_a_func(MathFunc call_this, int x, int y) 
-// {
-//     float output = call_this(x, y);
-//     return output;
-// }
-// int final_result = call_a_func(&mul, a, b);
-// cout<< a << " * " << b << " = " << final_result << "\n";
-
-
 char response[] = {'n', 'y'};
 enum {n, y};
 using namespace std;
