49b250b5822fdd92fcb223f3f4335ef4ee3a85c8
Bryan <kanubu@mail.uc.edu>
2016-01-18 11:23:18 -0500

    Added conversion from digit to string inside Case 2 submenu case 2

--- sandbox/addEm/addEm.cxx
+++ sandbox/addEm/addEm.cxx
@@ -142,6 +142,14 @@ int main()
 
                         if(string(userInputCategory) == "back" || string(userInputCategory) == "Back")
                             goto caseTwoSubMenu;
+                        else if(isdigit(userInputCategory[0]))
+                        {
+                            int x = atoi(userInputCategory);
+                            string retrievedCategoryValue = app.convertCategoryNumberToString(x);
+                            size_t length = retrievedCategoryValue.copy(userInputCategory, retrievedCategoryValue.length(), 0);
+                            userInputCategory[length]='\0';
+                            cout << "Category: " << userInputCategory << "\n";
+                        }
 
                         app.displaySpecificCategoryList(string(userInputCategory));
                         break;
