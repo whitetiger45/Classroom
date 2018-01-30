6504059f480a42a8c962b8993d180b859db3f6ed
Bryan <kanubu@mail.uc.edu>
2018-01-29 19:43:26 -0500

    Remove comment

--- sandbox/2018/systemSandbox_1.cxx
+++ sandbox/2018/systemSandbox_1.cxx
@@ -1,5 +1,4 @@
 // https://www.geeksforgeeks.org/system-call-in-c/
-// A C++ program that pauses screen at the end in Windows OS
 #include <iostream>
 #include <string>
 
@@ -10,7 +9,7 @@ int main ()
     string x = "echo \"Hello World\"";
 
     const char *command = x.c_str();
-    
+
     cout << "Running program...\n";
 
     system(command);
