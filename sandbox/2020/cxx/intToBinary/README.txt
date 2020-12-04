755f8a0c95d51ebc9aecffd2d4a355576cdcfc34
Bryan <kanubu@mail.uc.edu>
2020-12-04 17:55:11 -0500

    Quick base-10 to base-2 converter

---
+++ sandbox/2020/cxx/intToBinary/README.txt
@@ -0,0 +1,3 @@
+This is a program for quickly converting base-10 numbers to binary.
+
+Any bugs should reported to bryan.kanu18@gmail.com
---
+++ sandbox/2020/cxx/intToBinary/intToBinary.cc
@@ -0,0 +1,43 @@
+#include <array>
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <regex>
+#include <stdexcept>
+#include <string>
+
+using namespace std;
+
+// https://stackoverflow.com/questions/478898/how-do-i-execute-a-command-and-get-the-output-of-the-command-within-c-using-po
+string exec(const char* cmd) {
+    array<char, 128> buffer;
+    string result;
+    unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) {
+        throw runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
+}
+
+int main(int argc, char** argv){
+
+    if(argc != 2){
+        cout << "Usage: bin <base-10 integer to convert>\n";
+        exit(0);
+    }
+    
+    string userInputStr{argv[1]};
+    regex userInputRegExp("^(\\d+)$");
+    smatch m;
+    regex_match(userInputStr,m,userInputRegExp);
+
+    if(!m.empty()){
+        string cmd = "python3 -c 'print(\"{0:b}\".format("+m.str(1)+"))'";
+        cout << exec(const_cast<char*>(cmd.c_str()));
+    }else cout << "Could not find an integer in input.\n";
+
+    return 0;
+}
